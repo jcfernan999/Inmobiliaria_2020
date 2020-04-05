3b62033ba2bfed186b2639b3fffe98d0ef2d3f8a
Juan <jcfernan99@gmail.com>
2020-04-05 03:02:25 -0300

    Clase Propietario

---
+++ Inmobiliaria_2020/Models/Propietario.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inmobiliaria_2020.Models
+{
+    public class Propietario
+    {
+    }
+}
